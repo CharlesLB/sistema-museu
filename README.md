6a089034ae87bb488dfc124005576b0d8406dd52
CharlesLB <charles.braga.jf.cb@gmail.com>
2020-04-26 19:41:25 -0300

    .

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Sistema de Peixes
-Sistema Open Source do controle de peixes do laboratório de Biologia do IF Sudeste MG Campus Juiz de Fora
+Sistema PHP do museu do computador do IF Sudeste MG Campus Juiz de Fora
 
 ### Tecnologias
 
--- views/web/index.php
+++ views/web/index.php
@@ -7,77 +7,23 @@
     <link rel="stylesheet" href="../assets/css/style.css">
     <link rel="stylesheet" href="../assets/css/bootstrap.css">
     <link rel="stylesheet" href="../assets/css/globals.css">
-    <title>Sistema de Peixes</title>
+    <title>Museu do Computador</title>
 </head>
 
 <body>
     
-    <div class="home">
-        <div class="landing-text">
-            <h1>Laboratório de Biologia</h1>
-            <h3>Aqui a Viviane deverá escrever um textinho legal ou retirar este h3</h3>
-        </div>
-    </div>
-
-    <section id="intro">
-        <div class="padding">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-6">
-                        <img src="../assets/img/web/intro.png" alt="example">
-                    </div>
-                    <div class="col-sm-6 text-center">
-                        <h2>Nosso Projeto</h2>
-                        <p class="lead">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellendus itaque
-                            magnam quas. Maxime, ratione! Maxime tempore unde assumenda repudiandae dolore iusto aut
-                            voluptatem! Excepturi sed dignissimos odio hic laudantium sapiente.</p>
-                        <p class="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Labore assumenda
-                            cumque ex optio, porro alias amet deserunt praesentium, dolorem nisi maiores omnis
-                            repudiandae reprehenderit at itaque magnam, sequi quis. Id.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
-    <section id="login">
-        <div class="padding">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-6">
-                        <img src="../assets/img/web/intro.png" alt="example">
-                    </div>
-                    <div class="col-sm-6 d-flex align-items-center">
-                        <form>
-                            <h2>Login</h2>
-                            <div class="form-group">
-                                <label for="exampleInputEmail1">Matrícula</label>
-                                <input type="number" class="form-control" id="exampleInputEmail1"
-                                    aria-describedby="emailHelp">
-                            </div>
-                            <div class="form-group">
-                                <label for="exampleInputPassword1">Senha</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1">
-                            </div>
-                            <button type="submit" class="btn button">Login</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-
+   
     <footer>
         <div class="footer" id="footer">
             <div class="container">
                 <div class="row">
                     <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
-                        <h4> Laboratório de Biologia </h4>
+                        <h4> Museu do Computador </h4>
                     </div>
                     <div class="col-lg-3 col-sm-2 col-xs-3">
                         <h3> Contact </h3>
                         <ul>
-                            <li><a class="email" href="#">laboratoriobiologiaif@gmail.com</a></li>
+                            <li><a class="email" href="#">museudocomputadorif@gmail.com</a></li>
                             <br />
                             <li>
                                 <p> IF Sudeste MG Campus Juiz de Fora</p>
@@ -91,7 +37,7 @@
             </div>
             <div class="footer-bottom">
                 <div class="container">
-                    <p class="pull-left copyright"> <a href="https://github.com/CharlesLB" target="_blank">@CharlesLB </a> e Viviane Serafim </p>
+                    <p class="pull-left copyright"> <a href="https://github.com/CharlesLB" target="_blank">@CharlesLB </a></p>
 
                 </div>
             </div>
