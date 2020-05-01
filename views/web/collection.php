a9a3d5f66e3130f811ed2f27dd7cfaf9ceb9b945
CharlesLB <charles.braga.jf.cb@gmail.com>
2020-05-01 14:43:23 -0300

    Interface

--- views/web/collection.php
+++ views/web/collection.php
@@ -3,18 +3,29 @@
 <html>
 
 <head>
-	<title>Sistema do Museu</title>
+	<title>No Sidebar - Helios by HTML5 UP</title>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-	<link rel="icon" href="assets/images/icon.ico" type="image/x-icon" />
-	<link rel="stylesheet" href="assets/css/main.css?version=2" />
+	<link rel="stylesheet" href="assets/css/main.css?version=4158" />
+	<link rel="stylesheet" href="assets/css/lightbox.css">
 	<noscript>
 		<link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
 </head>
 
-<body class="homepage is-preload">
+<body class="no-sidebar is-preload">
 	<div id="page-wrapper">
+
+		<!-- Header -->
 		<div id="header">
+
+			<!-- Inner -->
+			<div class="inner">
+				<header>
+					<h1><a href="index.html" id="logo">Museu do Computador</a></h1>
+				</header>
+			</div>
+
+			<!-- Nav -->
 			<nav id="nav">
 				<ul>
 					<li><a href="index.html">Home</a></li>
@@ -23,35 +34,24 @@
 				</ul>
 			</nav>
 
-			<div class="inner">
-				<header>
-					<h1><a href="index.html" id="logo">Museu do computador</a></h1>
-					<hr />
-					<p>O acervo de informática do IF Sudeste MG Campus Juiz de Fora</p>
-				</header>
-				<footer>
-					<a href="#banner" class="button circled scrolly">Conhecer</a>
-				</footer>
-			</div>
-
-
 		</div>
 
+		<!-- Main -->
 		<section id="banner">
 			<header>
-				<h2>Conheça o <strong>Museu do Computador</strong>.</h2>
-				<p>
-					O museu do <a href="https://www.ifsudestemg.edu.br/juizdefora">IF Sudeste MG Campus Juiz de Fora</a>.
-					Veja abaixo algumas das peças de nosso vasto acervo
-				</p>
+				<h2>Conheça o <strong>nosso acervo</strong> completo.</h2>
 			</header>
 		</section>
 
+		
 		<section class="carousel">
-			<div class="reel">
+			<header class="center title">
+				<h2>Primeira categoria</h2>
+			</header>
 
+			<div class="reel">
 				<article>
-					<a href="images/pic01.jpg" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
+					<a href="images/pic01.jpg" class="image featured" data-lightbox="mygallery"><img src="images/pic01.jpg" alt="" /></a>
 					<header>
 						<h3><a href="#">Nome da peça</a></h3>
 					</header>
@@ -59,7 +59,7 @@
 				</article>
 
 				<article>
-					<a href="#" class="image featured"><img src="images/pic02.jpg" alt="" /></a>
+					<a href="images/pic01.jpg" class="image featured" data-lightbox="mygallery"><img src="images/pic02.jpg" alt="" /></a>
 					<header>
 						<h3><a href="#">Nome da peça</a></h3>
 					</header>
@@ -131,41 +131,10 @@
 				</article>
 
 			</div>
-
-			<footer class="center">
-				<a href="#" class="button">Conhecer todo o nosso acervo</a>
-			</footer>
 		</section>
-
-		<section class="section">
-			<header class="container">
-				<h2>O <strong>Museu do Computador</strong> precisa de você.</h2>
-				<p>
-					Caso possua uma peça de informática antiga que enriqueceria nosso acervo e está disposto a doar, entre em contato clicando no botão abaixo.
-				</p>
-			</header>
-			<footer class="center">
-				<a href="#contact" class="button scrolly">Doar uma peça</a>
-			</footer>
-		</section>
-
-
-
-		<!-- Main -->
-		<div class="wrapper style2">
-			<article id="main" class="container special">
-				<header>
-					<h2>E venha no conhecer!</h2>
-					<p>
-						Ficamos aberto terça e quinta-feira das 14:00 às 18:00 horas!
-					</p>
-				</header>
-				<div class="image featured"><img src="images/pic06.jpg" alt="" /></div>
-			</article>
-		</div>
-
 
 		<!-- Footer -->
+		
 		<div id="footer">
 			<div class="container">
 				<section id="contact">
@@ -238,7 +207,10 @@
 	<script src="assets/js/breakpoints.min.js"></script>
 	<script src="assets/js/util.js"></script>
 	<script src="assets/js/main.js"></script>
+	<script src="assets/js/lightbox-plus-jquery.min.js"></script>
 
 </body>
 
 </html>
+
+<!DOCTYPE HTML>
