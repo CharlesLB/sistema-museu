9bf4ee3e3ebeef74d3c0b8190479547b92d8b568
CharlesLB <charles.braga.jf.cb@gmail.com>
2020-05-22 17:41:46 -0300

    Interfaces

--- views/web/home.php
+++ views/web/home.php
@@ -1,19 +1,5 @@
-<!DOCTYPE HTML>
+<?php $v->layout("theme"); ?>
 
-<html>
-
-<head>
-	<title>Sistema do Museu</title>
-	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
-	<link rel="icon" href="assets/images/icon.ico" type="image/x-icon" />
-	<link rel="stylesheet" href="assets/css/main.css?version=3" />
-	<noscript>
-		<link rel="stylesheet" href="assets/css/noscript.css" /></noscript>
-</head>
-
-<body class="homepage is-preload">
-	<div id="page-wrapper">
 		<div id="header">
 			<nav id="nav">
 				<ul>
@@ -154,7 +140,7 @@
 		<section class="carousel">
 			<div class="reel">
 				<article>
-					<a href="images/pic01.jpg" class="image featured"><img src="images/pic01.jpg" alt="" /></a>
+					<a href="images/pic01.jpg" class="image featured"><img src=" images/pic01.jpg" alt="" /></a>
 					<header>
 						<h3><a href="#">Nome da peça</a></h3>
 					</header>
@@ -255,79 +241,15 @@
 		</div>
 
 
-		<!-- Footer -->
-		<div id="footer">
-			<div class="container">
-				<section id="contact">
-					<div class="center">
-						<h2>Faça sua doação!</h2>
-					</div>
-					<form action="#" method="post">
-						<div class="form-inline">
-							<div class="form-group">
-								<label for="name">Nome:</label>
-								<input type="text" id="name" placeholder="Nome Completo" required>
-							</div>
-							<div class="form-group">
-								<label for="email">E-mail:</label>
-								<input type="email" id="email" placeholder="E-mail" required>
-							</div>
-						</div>
-						<div class="form-inline">
-							<div class="form-group">
-								<label for="item">Qual peça gostaria de doar:</label>
-								<input type="text" id="item" placeholder="Peça a ser doada" required>
-							</div>
-							<div class="form-group">
-								<label for="date">Data para entrega preferencial:</label>
-								<input type="text" id="date" placeholder="É obrigatório ser dia útil" required>
-							</div>
-						</div>
-						<div class="form-group">
-							<label for="textarea">Descrição Opcional:</label>
-							<textarea id="textarea">Aqui você pode fazer qualquer pergunta ou explicar qualquer outro problema</textarea>
-						</div>
-						<footer class="center">
-							<button type="submit" class="button">Doar uma peça</button>
-						</footer>
-
-					</form>
-				</section>
-
-				<hr />
-				<div class="row">
-					<div class="col-12">
-
-						<!-- Contact -->
-						<section class="contact">
-							<header>
-								<h3>Museu do Computador</h3>
-							</header>
-							<p>Um projeto do IF Sudeste MG Campus Juiz de Fora</p>
-						</section>
-
-						<!-- Copyright -->
-						<div class="copyright">
-							<ul class="menu">
-								<li><a href="https://github.com/CharlesLB">@CharlesLB</a></li>
-								<li>Ícones feitos por <a href="https://www.flaticon.com/br/autores/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/br/" title="Flaticon"> www.flaticon.com</a></li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-
 	<!-- Scripts -->
-	<script src="assets/js/jquery.min.js"></script>
-	<script src="assets/js/jquery.dropotron.min.js"></script>
-	<script src="assets/js/jquery.scrolly.min.js"></script>
-	<script src="assets/js/jquery.scrollex.min.js"></script>
-	<script src="assets/js/browser.min.js"></script>
-	<script src="assets/js/breakpoints.min.js"></script>
-	<script src="assets/js/util.js"></script>
-	<script src="assets/js/main.js"></script>
+	<script src="<?= view("assets/js/jquery.min.js") ?>"></script>
+	<script src="<?= view("assets/js/jquery.dropotron.min.js") ?>"></script>
+	<script src="<?= view("assets/js/jquery.scrolly.min.js") ?>"></script>
+	<script src="<?= view("assets/js/jquery.scrollex.min.js") ?>"></script>
+	<script src="<?= view("assets/js/browser.min.js") ?>"></script>
+	<script src="<?= view("assets/js/breakpoints.min.js") ?>"></script>
+	<script src="<?= view("assets/js/util.js") ?>"></script>
+	<script src="<?= view("assets/js/main.js") ?>"></script>
 
 </body>
 
---
+++ views/web/theme.php
@@ -0,0 +1,96 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+	<title>Sistema do Museu</title>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<link rel="icon" href="<?= view("assets/images/icon.ico") ?> " type="image/x-icon" />
+	<link rel="stylesheet" href="<?= view("") ?> assets/css/main.css?version=3" />
+	<noscript>
+		<link rel="stylesheet" href="<?= view("") ?> assets/css/noscript.css" /></noscript>
+</head>
+
+<body class="homepage is-preload">
+	<div id="page-wrapper">
+
+        <?= $v->section("content"); ?>
+
+		<!-- Footer -->
+		<div id="footer">
+			<div class="container">
+				<section id="contact">
+					<div class="center">
+						<h2>Faça sua doação!</h2>
+					</div>
+					<form action="#" method="post">
+						<div class="form-inline">
+							<div class="form-group">
+								<label for="name">Nome:</label>
+								<input type="text" id="name" placeholder="Nome Completo" required>
+							</div>
+							<div class="form-group">
+								<label for="email">E-mail:</label>
+								<input type="email" id="email" placeholder="E-mail" required>
+							</div>
+						</div>
+						<div class="form-inline">
+							<div class="form-group">
+								<label for="item">Qual peça gostaria de doar:</label>
+								<input type="text" id="item" placeholder="Peça a ser doada" required>
+							</div>
+							<div class="form-group">
+								<label for="date">Data para entrega preferencial:</label>
+								<input type="text" id="date" placeholder="É obrigatório ser dia útil" required>
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="textarea">Descrição Opcional:</label>
+							<textarea id="textarea">Aqui você pode fazer qualquer pergunta ou explicar qualquer outro problema</textarea>
+						</div>
+						<footer class="center">
+							<button type="submit" class="button">Doar uma peça</button>
+						</footer>
+
+					</form>
+				</section>
+
+				<hr />
+				<div class="row">
+					<div class="col-12">
+
+						<!-- Contact -->
+						<section class="contact">
+							<header>
+								<h3>Museu do Computador</h3>
+							</header>
+							<p>Um projeto do IF Sudeste MG Campus Juiz de Fora</p>
+						</section>
+
+						<!-- Copyright -->
+						<div class="copyright">
+							<ul class="menu">
+								<li><a href="https://github.com/CharlesLB">@CharlesLB</a></li>
+								<li>Ícones feitos por <a href="https://www.flaticon.com/br/autores/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/br/" title="Flaticon"> www.flaticon.com</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Scripts -->
+	<script src="<?= view("assets/js/jquery.min.js") ?>"></script>
+	<script src="<?= view("assets/js/jquery.dropotron.min.js") ?>"></script>
+	<script src="<?= view("assets/js/jquery.scrolly.min.js") ?>"></script>
+	<script src="<?= view("assets/js/jquery.scrollex.min.js") ?>"></script>
+	<script src="<?= view("assets/js/browser.min.js") ?>"></script>
+	<script src="<?= view("assets/js/breakpoints.min.js") ?>"></script>
+	<script src="<?= view("assets/js/util.js") ?>"></script>
+	<script src="<?= view("assets/js/main.js") ?>"></script>
+
+</body>
+
+</html>
