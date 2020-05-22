a7e8d87e579ab5931c1cd20f9381019932b72eda
CharlesLB <charles.braga.jf.cb@gmail.com>
2020-05-22 17:41:08 -0300

    Router Controller

--- source/Controllers/Web.php
+++ source/Controllers/Web.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace Source\Controllers;
 
@@ -6,19 +6,27 @@ use Source\Core\Controller;
 
 class Web extends Controller
 {
-    public function __construct() 
-    {
-        parent::__construct(__DIR__."../../views/" );
+    public function __construct($router) {
+        parent::__construct($router);
     }
 
-    public function index(): void
-    {
-        echo "home";
+
+    // ─── HOME ───────────────────────────────────────────────────────────────────────
+
+    public function home()
+    {       
+        echo $this->view->render("web/home" , [
+            "title" => "Home | " . SITE["name"] 
+        ]);
     }
+
+    // ─── ERROR ──────────────────────────────────────────────────────────────────────
 
     public function error(array $data): void
     {
-        echo "Erro:";
-        var_dump($data);
+       echo $this->view->render("web/error", [
+            "title" => "Error | " . $data["errcode"],
+            "error" => $data["errcode"]
+        ]);
     }
 }
