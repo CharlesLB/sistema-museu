2d560bc189df1d279af729e11546dfb8b1cb7fea
CharlesLB <charles.braga.jf.cb@gmail.com>
2020-05-22 17:40:44 -0300

    Global Variables

--- source/Boot/Config.php
+++ source/Boot/Config.php
@@ -1,94 +1,41 @@
 <?php
 
 //
+// ─── URL ─────────────────────────────────────────────────────────────────────
+//
+
+
+define("URL", [
+    "localhost" => "https://www.localhost/sistema-museu",
+    "base" => "https://www.base.com"]
+);
+
+//
+// ─── SITE ───────────────────────────────────────────────────────────────────────
+//
+
+
+define("SITE", [
+    "name" => "Museu do computador",
+    "github" => "https://github.com/CharlesLB/sistema-museu"
+]);
+
+
+//
 // ─── DATABASE ───────────────────────────────────────────────────────────────────
 //
 
-    
-define("CONF_DB_HOST", "localhost");
-define("CONF_DB_USER", "root");
-define("CONF_DB_PASS", "");
-define("CONF_DB_NAME", "db_museum");
-
-//
-// ─── URL ────────────────────────────────────────────────────────────────────────
-//
-
-    
-define("CONF_URL_BASE", "https://www.sistemamuseu.com");
-define("CONF_URL_TEST", "https://www.localhost/sistema-museu");
-define("CONF_URL_ADMIN", "/admin");
-
-
-//
-// ─── DATE ───────────────────────────────────────────────────────────────────────
-//
-  
-define("CONF_DATE_BR", "d/m/Y H:i:s");
-define("CONF_DATE_APP", "Y-m-d H:i:s");
-
-
-//
-// ─── PASSWORD ───────────────────────────────────────────────────────────────────
-//
-
-define("CONF_PASSWD_MIN_LEN", 8);
-define("CONF_PASSWD_MAX_LEN", 40);
-define("CONF_PASSWD_ALGO", PASSWORD_DEFAULT);
-define("CONF_PASSWD_OPTION", ["cost" => 10]);
-
-//
-// ─── MESSAGE ────────────────────────────────────────────────────────────────────
-//
-
-    
-define("CONF_MESSAGE_CLASS", "trigger");
-define("CONF_MESSAGE_INFO", "info");
-define("CONF_MESSAGE_SUCCESS", "success");
-define("CONF_MESSAGE_WARNING", "warning");
-define("CONF_MESSAGE_ERROR", "error");
-
-//
-// ─── VIEW ───────────────────────────────────────────────────────────────────────
-//
-
-    
-define("CONF_VIEW_PATH", __DIR__ . "/../../shared/views");
-define("CONF_VIEW_EXT", "php");
-define("CONF_VIEW_THEME", "Admin");
-
-
-//
-// ─── UPLOAD ─────────────────────────────────────────────────────────────────────
-//
-
-
-define("CONF_UPLOAD_DIR", "../storage");
-define("CONF_UPLOAD_IMAGE_DIR", "images");
-define("CONF_UPLOAD_FILE_DIR", "files");
-define("CONF_UPLOAD_MEDIA_DIR", "medias");
-
-//
-// ─── IMAGES ─────────────────────────────────────────────────────────────────────
-//
-
-
-define("CONF_IMAGE_CACHE", CONF_UPLOAD_DIR . "/" . CONF_UPLOAD_IMAGE_DIR . "/cache");
-define("CONF_IMAGE_SIZE", 2000);
-define("CONF_IMAGE_QUALITY", ["jpg" => 75, "png" => 5]);
-
-//
-// ─── MAIL ───────────────────────────────────────────────────────────────────────
-//
-
-
-define("CONF_MAIL_HOST", "");
-define("CONF_MAIL_PORT", "");
-define("CONF_MAIL_USER", "");
-define("CONF_MAIL_PASS", "");
-define("CONF_MAIL_SENDER", ["name" => "Museu do Computador", "address" => "museudocomputador@gmail.com"]);
-define("CONF_MAIL_OPTION_LANG", "br");
-define("CONF_MAIL_OPTION_HTML", true);
-define("CONF_MAIL_OPTION_AUTH", true);
-define("CONF_MAIL_OPTION_SECURE", "tls");
-define("CONF_MAIL_OPTION_CHARSET", "utf-8");
+define("DATA_LAYER_CONFIG", [
+    "driver" => "mysql",
+    "host" => "localhost",
+    "port" => "3306",
+    "dbname" => "db_adedonha",
+    "username" => "root",
+    "passwd" => "",
+    "options" => [
+        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+        PDO::ATTR_CASE => PDO::CASE_NATURAL
+    ]
+]);
