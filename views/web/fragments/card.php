7a606284447657ac2304e419290ad63f318848d0
CharlesLB <charles.braga.jf.cb@gmail.com>
2020-05-23 13:49:44 -0300

    Fragments

---
+++ views/web/fragments/card.php
@@ -0,0 +1,7 @@
+<div class="card">
+    <a href="<?= asset("images/pic01.jpg", "web") ?>" class="image featured" data-lightbox="gallery"><img src="<?= asset("images/pic01.jpg", "web") ?>" alt="" /></a>
+    <header>
+        <h3><a href="#">Nome da peça</a></h3>
+    </header>
+    <p>Descrição bacana da peça.</p>
+</div>
---
+++ views/web/fragments/navbar.php
@@ -0,0 +1,8 @@
+<nav id="nav">
+    <ul>
+        <li><a href="<?= url() ?>">Home</a></li>
+        <li><a href="#contact" class="scrolly">Doação</a></li>
+        <li><a href="<?= url("/acervo") ?>">Acervo</a></li>
+        <li><a href="<?= url("/biblioteca") ?>">Biblioteca</a></li>
+    </ul>
+</nav>
